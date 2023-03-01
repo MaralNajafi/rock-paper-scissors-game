c84c47057dedab1e3735fc07274063e566625d1d
MaralNajafi <marawlnajafi@gmail.com>
2023-03-01 13:59:04 +0330

    print selections to DOM

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const DOMselectionButtons = document.querySelectorAll("[data-selection]");
+const DOMuserSelection = document.getElementById("userSelection");
+const DOMcomputerSelection = document.getElementById("computerSelection");
 
 const SELECTIONS = [
   {
@@ -24,35 +26,42 @@ DOMselectionButtons.forEach((selectionBtn) => {
 
     const userSelection = makeSelection(selectionName);
     const computerSelection = makeRandomSelection();
-    decideWinner(userSelection,computerSelection);
-    
+
+    printSelectionsToDOM(userSelection, DOMuserSelection);
+    printSelectionsToDOM(computerSelection, DOMcomputerSelection);
+
+    decideWinner(userSelection, computerSelection);
+
   });
 });
 
+function printSelectionsToDOM(selection, place) {
+  place.textContent = selection.sign;
+}
+
 function makeSelection(selectionName) {
-    const userSelection = SELECTIONS.find((selection) => {
-        return selection.name === selectionName;
-      });
-      console.log(`You: ${userSelection.sign}`);
-      return userSelection;
+  const userSelection = SELECTIONS.find((selection) => {
+    return selection.name === selectionName;
+  });
+
+  return userSelection;
 }
 
 function makeRandomSelection() {
   const randomSelectionIndex = Math.floor(Math.random() * SELECTIONS.length);
   const randomSelection = SELECTIONS[randomSelectionIndex];
-  console.log(`Computer: ${randomSelection.sign}`);
 
   return randomSelection;
 }
 
 function decideWinner(userSelection, computerSelection) {
-    if (userSelection.name === computerSelection.name) {
-        console.log("you draw");
-      } else {
-        if (userSelection.name === computerSelection.beats) {
-          console.log("you lost");
-        }else{
-          console.log("you won");
-        }
-      }
+  if (userSelection.name === computerSelection.name) {
+    console.log("you draw");
+  } else {
+    if (userSelection.name === computerSelection.beats) {
+      console.log("you lost");
+    } else {
+      console.log("you won");
+    }
+  }
 }
